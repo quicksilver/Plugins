a3c45b3d056b499cd7bac62d3f551dd28b7a64d8
Rob McBroom <github@skurfer.com>
2010-12-21 09:39:10 -0500

    removing completed TODO items

--- Cyberduck/Cyberduck.m
+++ Cyberduck/Cyberduck.m
@@ -5,8 +5,6 @@
 //  Created by Rob McBroom on 12/20/10.
 //
 
-// TODO add documentation to extendedDescription
-// TODO remove build directory before committing
 #import "Cyberduck.h"
 
 @implementation Cyberduck
